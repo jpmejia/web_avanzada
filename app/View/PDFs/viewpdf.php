59a5389f8d484ae8bcf9b0de5dadb6b6ac312ab1
jmejia <cromeocromeo@gmail.com>
2014-12-08 11:31:53 -0400

    [Cancion] hacer el crud para subir los archivos

--- app/View/PDFs/viewpdf.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $CakePdf = new CakePdf();
-    $CakePdf->template('newsletter', 'default');
-    //get the pdf string returned
-    $pdf = $CakePdf->output();
-    //or write it to file directly
-    $pdf = $CakePdf->write(APP . 'files' . DS . 'newsletter.pdf');
-?>
